89ca742fcf6729db6e716e0950fea0e0cd7c6e07
Precha Chainara <143707409+Precha-chang@users.noreply.github.com>
2024-06-26 12:09:57 +0700

    up2

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -14,15 +14,29 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Hello, my app"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("data1"),
+              Text("data2"),
+              Text("data3"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("data4"),
+              Text("data5"),
+              Text("data6"),
+            ],
+          ),
+        ],
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
